7d29c8baed9cf9213e0bb700ac3f13ddc4595dda
lena menezes <helena.macedo5@etec.sp.gov.br>
2023-08-03 23:20:44 -0300

    first commit

---
+++ nav.php
@@ -0,0 +1,40 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#"><img src="img/icon.png" alt="logo"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(página atual)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Novidades</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Pelúcias
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Harry Potter</a>
+          <a class="dropdown-item" href="#">Disney</a>
+          <a class="dropdown-item" href="#">Minecraft</a>
+          <a class="dropdown-item" href="#">Pokémon</a>
+          <a class="dropdown-item" href="#">Marvel</a>
+          <a class="dropdown-item" href="#">Hora de Aventura</a>
+          <a class="dropdown-item" href="#">Gravity Falls</a>
+          <a class="dropdown-item" href="#">Animes Geral</a>
+          <a class="dropdown-item" href="#"></a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Outras</a>
+        </div>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Buscar">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+    </form>
+    <a class="link_login" href="#"><img id="login" src="img/login.png" alt="login">  Logon</a>
+  </div>
+</nav>
